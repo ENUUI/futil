467bee83d013fb945c694d557d3c3ad9e2970913
chenfh <877302564@qq.com>
2024-05-17 11:17:12 +0800

    refactor: interface

--- futil_platform_interface/lib/futil_platform_interface.dart
+++ futil_platform_interface/lib/futil_platform_interface.dart
@@ -1,16 +1,20 @@
 library futil_platform_interface;
 
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 import 'msg/os_version.dart';
 export 'msg/os_version.dart';
+
+const MethodChannel _channel = MethodChannel('plugins.github.enuui/futil');
 
 abstract class FtlInterface extends PlatformInterface {
   FtlInterface() : super(token: _token);
 
   static final Object _token = Object();
 
-  static FtlInterface _instance = _Ftl();
+  static FtlInterface _instance = MethodChannelFtl();
 
   static FtlInterface get instance => _instance;
 
@@ -46,4 +50,48 @@ abstract class FtlInterface extends PlatformInterface {
   }
 }
 
-class _Ftl extends FtlInterface {}
+class MethodChannelFtl extends FtlInterface {
+  @visibleForTesting
+  MethodChannel get channel => _channel;
+
+  @override
+  Future<int> sdkInt() async {
+    final r = await _channel.invokeMethod<int>('skd_int');
+    if (r == null) {
+      throw PlatformException(
+        code: 'UNKNOWN',
+        message: 'Unable to get sdk int',
+      );
+    }
+    return r;
+  }
+
+  @override
+  Future<bool> isHarmonyOs() async {
+    final r = await _channel.invokeMethod<bool>('is_harmony_os');
+    if (r == null) {
+      throw PlatformException(
+        code: 'UNKNOWN',
+        message: 'Unable to get is harmony os',
+      );
+    }
+    return r;
+  }
+
+  @override
+  Future<OsVersion> osVersion() async {
+    final result = await _channel.invokeMapMethod('os_version');
+    if (result == null) {
+      throw PlatformException(
+        code: 'UNKNOWN',
+        message: 'Unable to get os version',
+      );
+    }
+    return OsVersion.fromJson(result.cast());
+  }
+
+  @override
+  Future<String> deviceId() async {
+    return await _channel.invokeMethod('device_id');
+  }
+}
